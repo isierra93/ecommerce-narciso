0a1a1071ea606fb56441724182ce0bc3325e4a3b
Ivan Sierra <sierraivan93@gmail.com>
2022-08-23 02:02:58 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./assets/Footer";
+import Header from "./assets/Header";
+import burga from "./assets/images/burga.png"
 
 function App() {
+  let titulo = `Bienvenido a Wendys!`
+  let description = `Dos carnes a la parrilla, pan, queso cheddar, cebolla, mostaza y ketchup.`;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <Header />
+      <div className="row">
+        <h1 className="text-center">{titulo}</h1>
+      </div>
+      <div className="row">
+        <img className="col-6" src={burga} alt="asd" />
+        <p className="col-6">{description}</p>
+        
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/assets/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () =>{
+    
+    return(
+        <footer>
+            <p>Derechos reservados 2022.</p>
+        </footer>
+
+    );
+
+
+};
+
+export default Footer;
