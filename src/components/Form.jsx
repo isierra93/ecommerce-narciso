b126976095fe0b8a7dc170b18489bf36e3ba0875
Ivan Sierra <sierraivan93@gmail.com>
2024-04-20 18:45:56 -0300

    add modal & redirect with mercadopago

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useCartContext } from "./CartContext";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
-import Checkout from "./Checkout";
+import { Wallet } from '@mercadopago/sdk-react'
+import axios from "axios";
 
 const Form = () => {
 
@@ -11,36 +12,65 @@ const Form = () => {
     const [telefono, setTelefono] = useState(``);
     const [email, setEmail] = useState(``);
     const [orderId, setOrderId] = useState(``);
+    const [preferenceIDSTATUS, setPreferenceIDSTATUS] = useState(null)
 
 
     const obtenerNombre = (e) => setNombre(e.target.value);
     const obtenerTelefono = (e) => setTelefono(e.target.value)
     const obtenerEmail = (e) => setEmail(e.target.value);
 
-    const generarOrden = (e) => {
-        e.preventDefault();
-        const orden = {
-            buyer: {
-                nombre: nombre,
-                telefono: telefono,
-                email: email
-            },
-            items: cart.map(item => ({ id: item.id, nombre: item.nombre, precio: item.precio })),
-            total: totalPrice()
+    const generarOrden = async () => {
+        try {
+            const orden = {
+                buyer: {
+                    nombre: nombre,
+                    telefono: telefono,
+                    email: email,
+                },
+                items: cart.map(item => ({ id: item.id, nombre: item.nombre, precio: item.precio })),
+                total: totalPrice()
+            }
+            const db = getFirestore();
+            const orderCollection = collection(db, `ordenes`)
+            const response = await addDoc(orderCollection, orden)
+            setOrderId(response.id)
+            clearCart()
+        } catch (error) {
+            console.error('Error:', error)
         }
-        const db = getFirestore();
-        const orderCollection = collection(db, `ordenes`);
-        addDoc(orderCollection, orden).then((res) => {
-            setOrderId(res.id);
-        });
-        clearCart();
+    }
+
+    const createPreference = async () => {
+        try {
+            const response = await axios.post('https://node-js-mercadopago.vercel.app/api/create-order', {
+                titleProducts: 'Narciso Store',
+                totalPrice: totalPrice(),
+                url_redirect: 'https://ecommerce-narciso.vercel.app/payed'
+            })
+
+            const { id } = response.data
+            return id
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const id = await createPreference()
+        if (id) {
+            setPreferenceIDSTATUS(id)
+            await generarOrden()
+        }
     }
 
 
     return (
         <div className="d-flex justify-content-center viewContainer">
             {orderId === "" ?
-                <form className="form-group d-flex flex-column mt-5 mb-5" action="" onSubmit={generarOrden}>
+                <form className="form-group d-flex flex-column mt-5 mb-5" action="" onSubmit={handleSubmit}>
                     <h1 className="text-center mb-5 mt-3">Formulario de compra</h1>
                     <input
                         className="mb-3 mt-3"
@@ -52,7 +82,7 @@ const Form = () => {
                         required
                     />
                     <input
-                    className="mb-3"
+                        className="mb-3"
                         type="number"
                         name="telefono"
                         placeholder="Telefono"
@@ -61,7 +91,7 @@ const Form = () => {
                         required
                     />
                     <input
-                    className="mb-3"
+                        className="mb-3"
                         type="email"
                         name="email"
                         placeholder="Email"
@@ -73,8 +103,23 @@ const Form = () => {
                         <button className="btn btn-success">Confirmar</button>
                     </div>
                 </form>
-                : <Checkout id={orderId} />
-            }</div>
+                :
+                <div className="container mt-5 mb-3 d-flex flex-column align-items-center">
+                    <div className="row text-center">
+                        <h2>Orden generada!</h2>
+                        <p>Su ID de compra es: <span className="text-success"><strong>{orderId}</strong></span></p>
+                        <h3><strong>Metodos de pago:</strong></h3>
+                    </div>
+            {
+                preferenceIDSTATUS &&
+                <Wallet initialization={{ preferenceId: preferenceIDSTATUS, redirectMode: 'modal' }} />
+            }
+                    <div className="row mt-2 img-checkout">
+                    <img className="rounded" src="https://plus.unsplash.com/premium_photo-1681492971778-dfe7e9d2f664?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Plants are friends" />
+                    </div>
+                </div>
+            }
+        </div>
     )
 };
 
