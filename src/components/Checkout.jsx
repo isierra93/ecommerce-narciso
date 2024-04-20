00ab5c4db1931dc3ee5c2783ef3cf204022bdce7
Ivan Sierra <sierraivan93@gmail.com>
2024-04-20 18:42:44 -0300

    add styles and rework of checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 
-const Checkout = ({id}) =>{
+const Checkout = () =>{
 
     return(
-        <div className="container mt-5 mb-3 d-flex flex-column align-items-center">
-            <div className="row text-center">
-                <h2>Compra generada!</h2>
-                <p>Gracias por su compra!</p>
-                <p>Su ID de compra es:</p>
-                <h3 className="text-success"><strong>{id}</strong></h3>
-            </div>
-            <div className="row mt-5 img-checkout">
-                <img className="rounded" src="https://plus.unsplash.com/premium_photo-1681492971778-dfe7e9d2f664?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Plants are friends" />
-            </div>
+        <div className="viewContainer container mt-5 payedContainer">
+            <section className="d-flex flex-column justify-content-center mt-5">
+                <h1 className="text-center">Gracias por su compra</h1>
+                <p className="text-center">Nos estaremos contactando para coordinar el envio.</p>
+            </section>
         </div>
     )
 };
